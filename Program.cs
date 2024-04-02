3c5a95996e41db3c5969c9393a9514c3cc943f49
Laxman Kc <A00298022@mycambrian.ca>
2024-04-02 19:30:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BookStore
+{
+    public class BookStoreProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the book store application.");
+        }
+    }
+}
